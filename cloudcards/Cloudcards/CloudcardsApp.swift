17d8d6ce4ee36e54f68b0181e010fa07fa291b2f
Silvia España <silviaespanagil@gmail.com>
2022-03-25 16:27:52 +0100

    Create new project

---
+++ cloudcards/Cloudcards/CloudcardsApp.swift
@@ -0,0 +1,17 @@
+//
+//  CloudcardsApp.swift
+//  Cloudcards
+//
+//  Created by Silvia España on 25/3/22.
+//
+
+import SwiftUI
+
+@main
+struct CloudcardsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
