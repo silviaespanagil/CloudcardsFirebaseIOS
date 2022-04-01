e2b2dcd156d02f59fe48fe455439690c9ed55d87
Silvia España <silviaespanagil@gmail.com>
2022-04-01 14:58:58 +0200

    Indent code Add Navigation View into the window group

--- cloudcards/Cloudcards/CloudcardsApp.swift
+++ cloudcards/Cloudcards/CloudcardsApp.swift
@@ -10,22 +10,26 @@ import Firebase
 
 class AppDelegate: NSObject, UIApplicationDelegate {
     
-  func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]? = nil) -> Bool {
-
-    FirebaseApp.configure()
-    return true
-  }
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]? = nil) -> Bool {
+        
+        FirebaseApp.configure()
+        return true
+    }
 }
 
 @main
 struct AppMain: App {
-  @UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
-  
-  var body: some Scene {
-      
-    WindowGroup {
+    
+    @UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
+    
+    var body: some Scene {
         
-      CardListView(viewModel:CardListViewModel())
+        WindowGroup {
+            
+            NavigationView {
+                
+                CardListView(viewModel:CardListViewModel())
+            }
+        }
     }
-  }
 }
