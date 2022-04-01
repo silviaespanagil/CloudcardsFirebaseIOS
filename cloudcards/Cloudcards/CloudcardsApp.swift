197fb54ad691f8314c8a745fe32f1a901c072265
Silvia España <silviaespanagil@gmail.com>
2022-04-01 14:32:47 +0200

    Update apps file to take firebase using an AppDelegate

--- cloudcards/Cloudcards/CloudcardsApp.swift
+++ cloudcards/Cloudcards/CloudcardsApp.swift
@@ -6,12 +6,26 @@
 //
 
 import SwiftUI
+import Firebase
+
+class AppDelegate: NSObject, UIApplicationDelegate {
+    
+  func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]? = nil) -> Bool {
+
+    FirebaseApp.configure()
+    return true
+  }
+}
 
 @main
-struct CloudcardsApp: App {
-    var body: some Scene {
-        WindowGroup {
-            ContentView()
-        }
+struct AppMain: App {
+  @UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
+  
+  var body: some Scene {
+      
+    WindowGroup {
+        
+      CardListView(viewModel:CardListViewModel())
     }
+  }
 }
