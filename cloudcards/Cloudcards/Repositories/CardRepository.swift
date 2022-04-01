e156b437bca0d063f89aeb61d5ca3fc22ad15702
Silvia España <silviaespanagil@gmail.com>
2022-04-01 14:33:33 +0200

    Create a card repository with getCards method

---
+++ cloudcards/Cloudcards/Repositories/CardRepository.swift
@@ -0,0 +1,40 @@
+//
+//  CardRepository.swift
+//  Cloudcards
+//
+//  Created by Silvia España on 31/3/22.
+//
+
+import FirebaseFirestore
+import FirebaseFirestoreSwift
+import Combine
+
+class CardRepository: ObservableObject {
+    
+    private let store = Firestore.firestore()
+    private let path = "cards"
+    @Published var cards: [Card] = []
+    
+    init() {
+        
+        getCards()
+    }
+    
+    func getCards() {
+        
+        store.collection(path)
+            .addSnapshotListener { querySnapshot, error in
+                
+                if let error = error {
+                    
+                    print("Error getting cards: \(error.localizedDescription)")
+                    return
+                }
+                
+                self.cards = querySnapshot?.documents.compactMap { document in
+                    
+                    try? document.data(as: Card.self)
+                } ?? []
+            }
+    }
+}
