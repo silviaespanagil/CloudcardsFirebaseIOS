3b29f6bcb4bb67051ebd0b46a8bacfff27712423
Silvia España <silviaespanagil@gmail.com>
2022-04-01 14:59:54 +0200

    Create Card List viewmodel with data map

---
+++ cloudcards/Cloudcards/Scenes/CardList/CardListViewModel.swift
@@ -0,0 +1,26 @@
+//
+//  CardListViewModel.swift
+//  Cloudcards
+//
+//  Created by Silvia España on 31/3/22.
+//
+
+import Foundation
+import Combine
+
+class CardListViewModel: ObservableObject {
+    
+    @Published var cardViewModels: [CardViewModel] = []
+    @Published var cardRepository = CardRepository()
+    
+    init() {
+        cardRepository.$cards.map { cards in
+            
+            cards.map { card in
+                
+                CardViewModel(card: card)
+            }
+        }.assign(to: &$cardViewModels)
+    }
+}
+    
