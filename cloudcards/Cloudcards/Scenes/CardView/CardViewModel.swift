782c22bf95e3417fca1ae53be7c349793721b9c4
Silvia España <silviaespanagil@gmail.com>
2022-04-12 13:00:27 +0200

    Update cardViewModel to actually update existing cards

--- cloudcards/Cloudcards/Scenes/CardView/CardViewModel.swift
+++ cloudcards/Cloudcards/Scenes/CardView/CardViewModel.swift
@@ -11,17 +11,34 @@ import Combine
 class CardViewModel: ObservableObject, Identifiable {
     
     @Published var card: Card
+    @Published var showContent = false
     
+    private var cardRepository: CardRepository
     private var cancellables: Set<AnyCancellable> = []
     var id = ""
     
-    init(card: Card) {
+    init(card: Card, cardRepository: CardRepository) {
         
         self.card = card
+        self.cardRepository = cardRepository
         
         $card
             .compactMap { $0.id }
             .assign(to: \.id, on: self)
             .store(in: &cancellables)
+    }
+    
+    func update(card: Card) {
+        
+        cardRepository.updateCard(card)
+    }
+    
+    func success(_ successful: Bool) {
+        
+        var updatedCard = card
+        
+        updatedCard.successful = successful
+        update(card: updatedCard)
+        showContent.toggle()
     }
 }
